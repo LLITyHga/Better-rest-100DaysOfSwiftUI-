d82990c8a3b1a7edd92b854edfc88f7c9a106920
Андрій Пікусь <llityhga@gmail.com>
2024-06-20 09:48:32 +0300

    Initial Commit

---
+++ Better rest/Better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_restApp.swift
+//  Better rest
+//
+//  Created by Wolf on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better rest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better rest
+//
+//  Created by Wolf on 20.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
